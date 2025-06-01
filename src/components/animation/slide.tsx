12d3bd146aa2db51a8206376ffb28e40abc08840
ACAZUMAH <calebazumah9@gmail.com>
2025-06-01 15:07:35 +0000

    changes

--- src/components/animation/slide.tsx
+++ src/components/animation/slide.tsx
@@ -1,53 +1,53 @@
-"use client";
-import { Group } from "@mantine/core";
-import { IconArrowLeft, IconArrowRight } from "@tabler/icons-react";
-import { AnimatePresence, motion, usePresenceData, wrap } from "motion/react";
-import { forwardRef, useState } from "react";
+// "use client";
+// import { Group } from "@mantine/core";
+// import { IconArrowLeft, IconArrowRight } from "@tabler/icons-react";
+// import { AnimatePresence, motion, usePresenceData, wrap } from "motion/react";
+// import { forwardRef, useState } from "react";
 
-export const DataSlider = () => {
-  const items = [1, 2, 3, 4, 5, 6];
-  const [selectedItem, setSelectedItem] = useState(items[0]);
-  const [direction, setDirection] = useState<1 | -1>(1);
+// export const DataSlider = () => {
+//   const items = [1, 2, 3, 4, 5, 6];
+//   const [selectedItem, setSelectedItem] = useState(items[0]);
+//   const [direction, setDirection] = useState<1 | -1>(1);
 
-  function setSlide(newDirection: 1 | -1) {
-    const nextItem = wrap(1, items.length, selectedItem + newDirection);
-    setSelectedItem(nextItem);
-    setDirection(newDirection);
-  }
+//   function setSlide(newDirection: 1 | -1) {
+//     const nextItem = wrap(1, items.length, selectedItem + newDirection);
+//     setSelectedItem(nextItem);
+//     setDirection(newDirection);
+//   }
 
-  return (
-    <>
-      <AnimatePresence custom={direction} initial={false} mode="popLayout">
-       {/* <Slide key={selectedItem} /> */}
-      </AnimatePresence>
+//   return (
+//     <>
+//       <AnimatePresence custom={direction} initial={false} mode="popLayout">
+//        {/* <Slide key={selectedItem} /> */}
+//       </AnimatePresence>
 
-      <Group justify="flex-end">
-        <motion.button
-          initial={false}
-          animate={{ backgroundColor: "#1f5de5" }}
-          aria-label="Previous"
-          style={button}
-          onClick={() => setSlide(-1)}
-          whileFocus={{ outline: `2px solid "#1f5de5"` }}
-          whileTap={{ scale: 0.9 }}
-        >
-          <IconArrowLeft style={{ color: "white" }} />
-        </motion.button>
-        <motion.button
-          initial={false}
-          animate={{ backgroundColor: "#1f5de5" }}
-          aria-label="Next"
-          style={button}
-          onClick={() => setSlide(1)}
-          whileFocus={{ outline: `2px solid "#1f5de5"` }}
-          whileTap={{ scale: 0.9 }}
-        >
-          <IconArrowRight style={{ color: "white" }} />
-        </motion.button>
-      </Group>
-    </>
-  );
-};
+//       <Group justify="flex-end">
+//         <motion.button
+//           initial={false}
+//           animate={{ backgroundColor: "#1f5de5" }}
+//           aria-label="Previous"
+//           style={button}
+//           onClick={() => setSlide(-1)}
+//           whileFocus={{ outline: `2px solid "#1f5de5"` }}
+//           whileTap={{ scale: 0.9 }}
+//         >
+//           <IconArrowLeft style={{ color: "white" }} />
+//         </motion.button>
+//         <motion.button
+//           initial={false}
+//           animate={{ backgroundColor: "#1f5de5" }}
+//           aria-label="Next"
+//           style={button}
+//           onClick={() => setSlide(1)}
+//           whileFocus={{ outline: `2px solid "#1f5de5"` }}
+//           whileTap={{ scale: 0.9 }}
+//         >
+//           <IconArrowRight style={{ color: "white" }} />
+//         </motion.button>
+//       </Group>
+//     </>
+//   );
+// };
 
 // const Slide = forwardRef(function Slide(
 //   ref: React.Ref<HTMLDivElement>
@@ -92,15 +92,15 @@ export const DataSlider = () => {
 //   borderRadius: "10px",
 // };
 
-const button: React.CSSProperties = {
-  backgroundColor: "#1f5de5",
-  width: 70,
-  height: 40,
-  borderRadius: "4rem",
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
-  position: "relative",
-  zIndex: 1,
-  outlineOffset: 2,
-};
+// const button: React.CSSProperties = {
+//   backgroundColor: "#1f5de5",
+//   width: 70,
+//   height: 40,
+//   borderRadius: "4rem",
+//   display: "flex",
+//   justifyContent: "center",
+//   alignItems: "center",
+//   position: "relative",
+//   zIndex: 1,
+//   outlineOffset: 2,
+// };
