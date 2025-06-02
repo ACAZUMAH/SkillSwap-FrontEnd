a5f5c9fd50a3d00af7948581bd043d957c08d475
ACAZUMAH <calebazumah9@gmail.com>
2025-06-02 01:14:54 +0000

    ci intergration

--- src/app/__tests__/App.test.tsx
+++ src/app/__tests__/App.test.tsx
@@ -1,13 +1,8 @@
 import { render } from "@testing-library/react";
 import App from "../App";
-import { BrowserRouter } from "react-router-dom";
 
 test("renders the app landing page", () => {
   window.history.pushState({}, "Test Page", "/");
-  const { asFragment } = render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  );
+  const { asFragment } = render(<App />);
   expect(asFragment()).toMatchSnapshot();
 });
