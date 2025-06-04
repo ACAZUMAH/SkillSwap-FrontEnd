0f31c9bf1d698f5c33e14db22061bb6a83719255
Azumah Asakwin Caleb <132952460+ACAZUMAH@users.noreply.github.com>
2025-06-04 20:18:43 +0000

    Update routes.tsx

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -2,8 +2,6 @@ import { routerEndPoints } from "src/constants";
 import { LandingLayout } from "src/layouts/Landing";
 import { Authentication } from "src/authentication";
 import type { RouteObject } from "react-router-dom";
-import Login from "src/authentication/components/Login";
-import Register from "src/authentication/components/Register";
 import { LandingPage } from "src/layouts/Landing/components";
 
 export const routes = [
