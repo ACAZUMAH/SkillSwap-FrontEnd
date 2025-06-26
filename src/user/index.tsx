b75f4d75b1d13b84738811075cd3bccfda366c18
ACAZUMAH <calebazumah9@gmail.com>
2025-06-26 23:37:08 +0000

    loader and sending swap request

--- src/user/index.tsx
+++ src/user/index.tsx
@@ -1,4 +1,4 @@
-import { Container, Group } from "@mantine/core";
+import { Alert, Container, Group } from "@mantine/core";
 import React from "react";
 import { BasicInfo } from "./components/BasicInfo";
 import { useParams } from "react-router-dom";
@@ -9,26 +9,43 @@ import { EducationInfo } from "./components/EducationInfo";
 import { RatingsAndReviews } from "./components/RatingsAndReviews";
 import { Avalability } from "./components/Avalability";
 import { useGetSwapQuery } from "./hooks/useGetSwapQuery";
+import { Conditional } from "src/components";
+import { UserLoader } from "./components/UserLoader";
 
 export const UserDetails: React.FC = () => {
   const param = useParams();
   const { user, loading, error } = useGetUserQuery(param.id!);
-  const { swap, loading: swapLoading } = useGetSwapQuery(param.id!);
+  const { swap } = useGetSwapQuery(param.id!);
+
+  const showDetails = !loading && !error && user;
+  const showLoading = loading && !error;
+  const showError = Boolean(error) && !loading;
 
   return (
     <Container w="100%" maw={1400} py={40}>
-      <Group align="flex-start" gap={35} className={classes.group}>
-        <div className={classes.basic}>
-          <BasicInfo user={user} swapData={swap}/>
-        </div>
-        <div className={classes.other}>
-          <SkillsInfo user={user} />
+      <Conditional condition={Boolean(showDetails)}>
+        <Group align="flex-start" gap={35} className={classes.group}>
+          <div className={classes.basic}>
+            <BasicInfo user={user} swapData={swap} />
+          </div>
+          <div className={classes.other}>
+            <SkillsInfo user={user} />
 
-          <EducationInfo educationalInfo={user?.education!}/>
+            <EducationInfo educationalInfo={user?.education!} />
 
-          <Avalability available={user?.availability!} />
-        </div>
-      </Group>
+            <Avalability available={user?.availability!} />
+          </div>
+        </Group>
+      </Conditional>
+      <Conditional condition={showLoading}>
+        <UserLoader />
+      </Conditional>
+      <Conditional condition={showError}>
+        <Alert title="Error" color="red">
+          <p>Something went wrong while fetching user details.</p>
+          <p>Please try again later.</p>
+        </Alert>
+      </Conditional>
       <RatingsAndReviews />
     </Container>
   );
